aff1453f03d4180394fe09dbe0c1d4f9d40bec07
Aditya Choudhary <cjaditya7@gmail.com>
2026-08-12 16:52:42 +0530

    Added Count Occurrences in a Sorted Array

---
+++ DSA-Striver/BinarySearch/Count_Occurrences_in_a_Sorted_Array.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int lastbins(vector<int>& nums, int target, int low, int high, int last){
+        int mid=low + (high - low)/2;
+        if(low>high){
+            return last;
+        }
+        
+        if(nums[mid]==target){
+            return lastbins(nums,target,mid+1,high, mid);
+        }
+        else if(nums[mid]>target){
+            return lastbins(nums,target,low,mid-1, last);
+        }
+        else{
+            return lastbins(nums,target,mid+1,high, last);
+        } 
+        
+    }
+    int firstbins(vector<int>& nums, int target, int low, int high, int first){
+        int mid=low + (high - low)/2;
+        if(low>high){
+            return first;
+        }
+        
+        if(nums[mid]==target){
+            return firstbins(nums,target,low,mid-1, mid);
+        }
+        else if(nums[mid]>target){
+            return firstbins(nums,target,low,mid-1, first);
+        }
+        else{
+            return firstbins(nums,target,mid+1,high, first);
+        } 
+        
+    }
+    int countOccurrences(vector<int>& nums, int target) {
+        if(nums.empty()){
+            return 0;   
+        }
+        int firstpos = firstbins(nums,target,0,nums.size()-1, -1);
+        if(firstpos==-1){
+            return 0;
+        }
+        int lastpos = lastbins(nums,target,0,nums.size()-1, -1);
+        return lastpos-firstpos+1;
+
+        
+    }
+};

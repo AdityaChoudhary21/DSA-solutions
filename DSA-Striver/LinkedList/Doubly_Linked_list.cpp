c129ca1e05bd06783e249883c3bc82db6ed995ee
Aditya Choudhary <cjaditya7@gmail.com>
2026-08-24 16:32:33 +0530

    Added Doubly Linked List

---
+++ DSA-Striver/LinkedList/Doubly_Linked_list.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    
+    public:
+    Node(int data1,Node* next1, Node* back1){
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+Node* ConvertArrtoDLL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2, 5 , 8 , 7};
+    Node* head = ConvertArrtoDLL(arr);
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    return 0;
+}

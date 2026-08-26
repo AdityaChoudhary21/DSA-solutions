51f1c30b446e22a6fff560d4a21ce49b8a28e1dc
Aditya Choudhary <cjaditya7@gmail.com>
2026-08-26 17:34:26 +0530

    Added Middle of the Linked List

---
+++ DSA-Striver/LinkedList/Middle_of_the_Linked_List.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* last = head;
+        ListNode* start = head;
+        while(start != NULL && start->next != NULL)
+        {
+            start=start->next->next;
+            last=last->next;
+        }
+        return last;
+        
+    }
+};
